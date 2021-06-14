13b025ca0ee8c9ac6e79a7505a3f246f5743e661
enya <ryuusei.y@icloud.com>
2021-06-14 17:22:49 +0900

    imgの修正

--- database/seeders/StoreTableSeeder.php
+++ database/seeders/StoreTableSeeder.php
@@ -19,7 +19,7 @@ class StoreTableSeeder extends Seeder
             'region' => '東京都',
             'genre' => '寿司',
             'overview' => '料理長厳選の食材から作る寿司を用いたコースをぜひお楽しみください。食材・味・価格、お客様の満足度を徹底的に追及したお店です。特別な日のお食事、ビジネス接待まで気軽に使用することができます。',
-            'image' => require("../assets/store/0.jpg")
+            'image' => '../assets/store/0.jpg'
         ];
         DB::table('stores')->insert($param);
 
@@ -28,7 +28,7 @@ class StoreTableSeeder extends Seeder
             'region' => '大阪府',
             'genre' => '焼肉',
             'overview' => '焼肉業界で20年間経験を積み、肉を熟知したマスターによる実力派焼肉店。長年の実績とお付き合いをもとに、なかなか食べられない希少部位も仕入れております。また、ゆったりとくつろげる空間はお仕事終わりの一杯や女子会にぴったりです。',
-            'image' => require("../assets/store/1.jpg")
+            'image' => '../assets/store/1.jpg',
         ];
         DB::table('stores')->insert($param);
 
@@ -37,7 +37,7 @@ class StoreTableSeeder extends Seeder
             'region' => '福岡県',
             'genre' => '居酒屋',
             'overview' => '気軽に立ち寄れる昔懐かしの大衆居酒屋です。キンキンに冷えたビールを、なんと199円で。鳥かわ煮込み串は販売総数100000本突破の名物料理です。仕事帰りに是非御来店ください。',
-            'image' => require("../assets/store/2.jpg"),
+            'image' => '../assets/store/2.jpg',
         ];
         DB::table('stores')->insert($param);
 
@@ -46,7 +46,7 @@ class StoreTableSeeder extends Seeder
             'region' => '東京都',
             'genre' => 'イタリアン',
             'overview' => '都心にひっそりとたたずむ、古民家を改築した落ち着いた空間です。イタリアで修業を重ねたシェフによるモダンなイタリア料理とソムリエセレクトによる厳選ワインとのペアリングが好評です。ゆっくりと上質な時間をお楽しみください。',
-            'image' => require("../assets/store/3.jpg"),
+            'image' => '../assets/store/3.jpg',
         ];
         DB::table('stores')->insert($param);
 
@@ -55,7 +55,7 @@ class StoreTableSeeder extends Seeder
             'region' => '福岡県',
             'genre' => 'ラーメン',
             'overview' => 'ラーメン屋とは思えない店内にはカウンター席はもちろん、個室も用意してあります。ラーメンはこってり系・あっさり系ともに揃っています。その他豊富な一品料理やアルコールも用意しており、居酒屋としても利用できます。ぜひご来店をお待ちしております。',
-            'image' => require("../assets/store/4.jpg"),
+            'image' => '../assets/store/4.jpg',
         ];
         DB::table('stores')->insert($param);
 
@@ -64,7 +64,7 @@ class StoreTableSeeder extends Seeder
             'region' => '東京都',
             'genre' => '焼肉',
             'overview' => '大小さまざまなお部屋をご用意してます。デートや接待、記念日や誕生日など特別な日にご利用ください。皆様のご来店をお待ちしております。',
-            'image' => require("../assets/store/5.jpg"),
+            'image' => '../assets/store/5.jpg',
         ];
         DB::table('stores')->insert($param);
 
