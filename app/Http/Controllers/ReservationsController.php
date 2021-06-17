a1e538e16663125621d000b3e119073f6bf6679a
enya <ryuusei.y@icloud.com>
2021-06-17 15:44:01 +0900

    datetimeの分解処理を追加

--- app/Http/Controllers/ReservationsController.php
+++ app/Http/Controllers/ReservationsController.php
@@ -30,9 +30,22 @@ class ReservationsController extends Controller
     public function get(Request $request)
     {
         $reservations_use_userid = DB::table('Reservations')->where('user_id', $request->user_id)->get();
+        $params = [
+            "id" => $reservations_use_userid::select('id')->get(),
+            "user_id" => $reservations_use_userid::select('user_id')->get(),
+            "store_id" => $reservations_use_userid::select('store_id')->get(),
+            "date" => date('Y-m-d',
+                $reservations_use_userid::select('day')->get()),
+            "time" => date('H:i',
+                $reservations_use_userid::select('day')->get()
+            ),
+            "number" => $reservations_use_userid::select('number')->get(),
+            "created_at" => $reservations_use_userid::select('created_at')->get(),
+            "updated_at" => $reservations_use_userid::select('updated_at')->get(),
+        ];
         return response()->json([
             'message' => 'Reservation got successfully',
-            'data' => $reservations_use_userid
+            'data' => $params
         ], 200);
     }
 
